307ae46b6a1557f59df2bfc783d6cb999280a68a
Stan-Balabushevich <stasntnew@gmail.com>
2022-10-01 17:59:43 +0200

    search view added. topbar not finished

--- app/src/main/java/id/slava/nt/simplenotepad/MainActivity.kt
+++ app/src/main/java/id/slava/nt/simplenotepad/MainActivity.kt
@@ -1,15 +1,30 @@
 package id.slava.nt.simplenotepad
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import id.slava.nt.simplenotepad.ui.ExpandableSearchView
 import id.slava.nt.simplenotepad.ui.theme.SimpleNotepadTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,24 +32,41 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SimpleNotepadTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+
+                ListScreen()
+
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun ListScreen(){
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                backgroundColor = MaterialTheme.colors.primary
+            ) {
+                ExpandableSearchView(
+                    searchDisplay = "",
+                    onSearchDisplayChanged = { },
+                    onSearchDisplayClosed = { })
+            }
+        },
+
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(28.dp),
+                onClick = { /* ... */ }) {
+                Icon(Icons.Filled.Add, contentDescription = "Localized description")
+
+            }
+        }
+    ) {
+       
+    }
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SimpleNotepadTheme {
-        Greeting("Android")
-    }
-}
